14976b09a4864e401b4c44a1fcb235cd35e14d3c
pinhotech_lucass <lucas.almeida@pinho.tech>
2023-10-05 20:01:41 -0300

    Fix/gravar

--- public/js/Hospedes/criar.js
+++ public/js/Hospedes/criar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-    var btnGravar = document.getElementById("btnGravarHospede");
+    var btnGravar = document.getElementById("btnGravarHospedes");
 
 
     btnGravar.addEventListener("click", function() {
@@ -13,49 +13,37 @@ function gravarHospede() {
 
     limparErros();
     
-    var inputNome = document.getElementById("inputNome");
-    var inputEmail = document.getElementById("inputEmail");
-    var inputSenha = document.getElementById("inputSenha");
-    var inputConfSenha = document.getElementById("inputConfSenha");
-    var selPerfil = document.getElementById("selPerfil");
-    var cbAtivo = document.getElementById("cbAtivo");
+    var Nome = document.getElementById("Nome");
+    var nasc = document.getElementById("nasc");
+    var CPF = document.getElementById("CPF");
+    var CEP = document.getElementById("CEP");
 
     var listaErros = [];
 
-    if(inputNome.value == "" || inputNome.value == undefined || inputNome.value == null){
-        listaErros.push("inputNome");
+    if(Nome.value == "" || Nome.value == undefined || Nome.value == null){
+        listaErros.push("Nome");
     }
     
-    if(inputEmail.value == "" || inputEmail.value == undefined || inputEmail.value == null){
-        listaErros.push("inputEmail");
+    if(nasc.value == "" || nasc.value == undefined || nasc.value == null){
+        listaErros.push("nasc");
     }
 
-    if(inputSenha.value == "" || inputSenha.value == undefined || inputSenha.value == null){
-        listaErros.push("inputSenha");
+    if(CPF.value == "" || CPF.value == undefined || CPF.value == null){
+        listaErros.push("CPF");
     }
 
-    if(inputConfSenha.value == "" || inputConfSenha.value == undefined || inputConfSenha.value == null){
-        listaErros.push("inputConfSenha");
+    if(CEP.value == "" || CEP.value == undefined || CEP.value == null){
+        listaErros.push("CEP");
     }
 
-    if(selPerfil.value == '0'){
-        listaErros.push("selPerfil");
-    }
-
-    if(inputSenha.value != inputConfSenha.value){
-        listaErros.push("inputSenha");
-        listaErros.push("inputConfSenha");
-    }
 
     if(listaErros.length == 0){
 
         var data = {
-            nome: inputNome.value,
-            email: inputEmail.value,
-            senha: inputSenha.value,
-            confSenha: inputConfSenha.value,
-            perfilId: selPerfil.value,
-            ativo: cbAtivo.checked
+            nome: Nome.value,
+            dataNasc: nasc.value,
+            hospedeCPF: CPF.value,
+            hospedeCEP: CEP.value,
         };
 
         fetch('/hospedes/criar', { 
@@ -65,23 +53,20 @@ function gravarHospede() {
             },
             body: JSON.stringify(data)
         })
-        .then(r=> {
-            return r.json();
-        })
+        
         .then(r=> {          
             if(r.ok) {
-                inputNome.value = "";
-                inputEmail.value = "";
-                inputSenha.value = "";
-                inputConfSenha.value = "";
-                selPerfil.value = 0;
-                cbAtivo.checked = false;
+                Nome.value = "";
+                nasc.value = "";
+                CPF.value = "";
+                CEP.value = "";
+               
 
-                document.getElementById("alertaSucesso").innerText = "Usuário gravado com sucesso!";
+                document.getElementById("alertaSucesso").innerText = "Hospede gravado com sucesso!";
                 document.getElementById("alertaSucesso").style = "display:block";
             }
             else{
-                document.getElementById("erros").innerText = "Erro ao gravar usuário!";
+                document.getElementById("erros").innerText = "Erro ao gravar hospede!";
                 document.getElementById("erros").style = "display:block";
             }
         })
@@ -108,11 +93,11 @@ function mostrarErros(lista) {
 }
 
 function limparErros() {
-    document.getElementById("inputNome").classList.remove("campoErro");
-    document.getElementById("inputEmail").classList.remove("campoErro");
-    document.getElementById("inputSenha").classList.remove("campoErro");
-    document.getElementById("inputConfSenha").classList.remove("campoErro");
-    document.getElementById("selPerfil").classList.remove("campoErro");
+    document.getElementById("Nome").classList.remove("campoErro");
+    document.getElementById("nasc").classList.remove("campoErro");
+    document.getElementById("CPF").classList.remove("campoErro");
+    document.getElementById("CEP").classList.remove("campoErro");
+  
 
     document.getElementById("erros").style = "display:none";
     document.getElementById("alertaSucesso").style = "display:none";
