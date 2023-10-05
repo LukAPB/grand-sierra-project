be2deebc51985355a8257f2b8167612c7f21f579
giihsatiko <giovana_satiko@hotmail.com>
2023-10-05 20:28:35 -0300

    fix: alterar js

--- public/js/Hospedes/alterar.js
+++ public/js/Hospedes/alterar.js
@@ -13,51 +13,49 @@ function alterarHospede() {
 
     limparErros();
     
-    var inputId = document.getElementById("inputId");
-    var inputNome = document.getElementById("inputNome");
-    var inputEmail = document.getElementById("inputEmail");
-    var inputSenha = document.getElementById("inputSenha");
-    var inputConfSenha = document.getElementById("inputConfSenha");
-    var selPerfil = document.getElementById("selPerfil");
-    var cbAtivo = document.getElementById("cbAtivo");
+    var Nome = document.getElementById("Nome");
+    var nasc = document.getElementById("nasc");
+    var CPF = document.getElementById("CPF");
+    var CEP = document.getElementById("CEP");
+    var checkin = document.getElementById("checkin")
+    var checkout = document.getElementById("checkout");
+
 
     var listaErros = [];
 
-    if(inputNome.value == "" || inputNome.value == undefined || inputNome.value == null){
-        listaErros.push("inputNome");
+    if(Nome.value == "" || Nome.value == undefined || Nome.value == null){
+        listaErros.push("Nome");
     }
     
-    if(inputEmail.value == "" || inputEmail.value == undefined || inputEmail.value == null){
-        listaErros.push("inputEmail");
+    if(nasc.value == "" || nasc.value == undefined || nasc.value == null){
+        listaErros.push("nasc");
     }
 
-    if(inputSenha.value == "" || inputSenha.value == undefined || inputSenha.value == null){
-        listaErros.push("inputSenha");
+    if(CPF.value == "" || CPF.value == undefined || CPF.value == null){
+        listaErros.push("CPF");
     }
 
-    if(inputConfSenha.value == "" || inputConfSenha.value == undefined || inputConfSenha.value == null){
-        listaErros.push("inputConfSenha");
+    if(CEP.value == "" || CEP.value == undefined || CEP.value == null){
+        listaErros.push("CEP");
     }
 
-    if(selPerfil.value == '0'){
-        listaErros.push("selPerfil");
+    if(checkin.value == "" || checkin.value == undefined || checkin.value == null){
+        listaErros.push("checkin");
     }
 
-    if(inputSenha.value != inputConfSenha.value){
-        listaErros.push("inputSenha");
-        listaErros.push("inputConfSenha");
+    if(checkout.value == "" || checkout.value == undefined || checkout.value == null){
+        listaErros.push("checkout");
     }
 
     if(listaErros.length == 0){
 
         var data = {
-            id: inputId.value,
-            nome: inputNome.value,
-            email: inputEmail.value,
-            senha: inputSenha.value,
-            confSenha: inputConfSenha.value,
-            perfilId: selPerfil.value,
-            ativo: cbAtivo.checked
+            nome: Nome.value,
+            dataNasc: nasc.value,
+            hospedeCPF: CPF.value,
+            hospedeCEP: CEP.value,
+            hospedeCheckin: checkin.value,
+            hospedeCheckout: checkout.value
         };
 
         fetch('/hospedes/alterar', { 
@@ -104,12 +102,12 @@ function mostrarErros(lista) {
 }
 
 function limparErros() {
-    document.getElementById("inputNome").classList.remove("campoErro");
-    document.getElementById("inputEmail").classList.remove("campoErro");
-    document.getElementById("inputSenha").classList.remove("campoErro");
-    document.getElementById("inputConfSenha").classList.remove("campoErro");
-    document.getElementById("selPerfil").classList.remove("campoErro");
-
+    document.getElementById("Nome").classList.remove("campoErro");
+    document.getElementById("nasc").classList.remove("campoErro");
+    document.getElementById("CPF").classList.remove("campoErro");
+    document.getElementById("CEP").classList.remove("campoErro");
+    document.getElementById("checkin").classList.remove("campoErro");
+    document.getElementById("checkout").classList.remove("campoErro");
     document.getElementById("erros").style = "display:none";
     document.getElementById("alertaSucesso").style = "display:none";
 }
