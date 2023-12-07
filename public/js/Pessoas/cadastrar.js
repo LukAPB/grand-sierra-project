30fb29c860fe90b46315e33c1b6411caff9e6498
vpireschaves <vpireschaves@gmail.com>
2023-12-06 21:25:59 -0300

    :sparkles: feat: validação de cadastro de pessoas

--- public/js/Pessoas/cadastrar.js
+++ public/js/Pessoas/cadastrar.js
@@ -5,12 +5,125 @@ document.addEventListener("DOMContentLoaded", function(){
     btnGravar.addEventListener("click", gravarPessoa);
 
     var razaoSocial = document.getElementById("razaoSocial");
-    var email = document.getElementById("email");
     var CEP = document.getElementById("CEP");
     var logradouro = document.getElementById("logradouro");
     var CNPJ = document.getElementById("CNPJ");
     var numTelefone = document.getElementById("numTelefone");
+
+    razaoSocial.addEventListener("input", function() {
+        if (razaoSocial.value.length > 100) {
+            razaoSocial.value = razaoSocial.value.slice(0, 100);
+        }
+    });
+
+    CEP.addEventListener("input", function() {
+        var cep = CEP.value.replace(/\D/g, '');
+        if (cep.length > 8) {
+            cep = cep.slice(0, 8);
+        }
+        CEP.value = cep;
+        if (cep.length === 8) {
+            fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.logradouro) {
+                        logradouro.value = data.logradouro;
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    });
+
+    logradouro.addEventListener("input", function() {
+        if (logradouro.value.length > 100) {
+            logradouro.value = logradouro.value.slice(0, 100);
+        }
+    });
+
+    CNPJ.addEventListener("input", function() {
+        CNPJ.value = CNPJ.value.replace(/\D/g, '');
+        if (CNPJ.value.length > 14) {
+            CNPJ.value = CNPJ.value.slice(0, 14);
+        }
+        if (CNPJ.value.length === 14) {
+            if (!validarCNPJ(CNPJ.value)) {
+                alert("CNPJ inválido!");
+                CNPJ.value = "";
+            }
+        }
+    });
+
+    numTelefone.addEventListener("input", function() {
+        numTelefone.value = numTelefone.value.replace(/\D/g, '');
+        if (numTelefone.value.length > 15) {
+            numTelefone.value = numTelefone.value.slice(0, 15);
+        }
+    });
+
+    window.onload = function(){
+        var inputs = document.getElementsByTagName("input");
+        for(var i = 0; i < inputs.length; i++){
+            inputs[i].value = "";
+        }
+    };
+
+    function validarCNPJ(cnpj) {
+        cnpj = cnpj.replace(/[^\d]+/g, '');
+
+        if (cnpj.length !== 14) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (/^(\d)\1+$/.test(cnpj)) {
+            return false;
+        }
+
+        // Validação do primeiro dígito verificador
+        let sum = 0;
+        let weight = 2;
+
+        for (let i = 11; i >= 0; i--) {
+            sum += parseInt(cnpj.charAt(i)) * weight;
+            weight = weight === 9 ? 2 : weight + 1;
+        }
+
+        let mod = sum % 11;
+        let digit = mod < 2 ? 0 : 11 - mod;
+
+        if (parseInt(cnpj.charAt(12)) !== digit) {
+            return false;
+        }
+
+        // Validação do segundo dígito verificador
+        sum = 0;
+        weight = 2;
+
+        for (let i = 12; i >= 0; i--) {
+            sum += parseInt(cnpj.charAt(i)) * weight;
+            weight = weight === 9 ? 2 : weight + 1;
+        }
+
+        mod = sum % 11;
+        digit = mod < 2 ? 0 : 11 - mod;
+
+        if (parseInt(cnpj.charAt(13)) !== digit) {
+            return false;
+        }
+
+        return true;
+    }
+    
 })
+
+function validarEmail(email) {
+    // Verifica se o email possui o formato correto
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 
 function gravarPessoa() {
 
@@ -22,9 +135,10 @@ function gravarPessoa() {
     var numTelefone = document.getElementById("numTelefone");
 
     //if de validação básica
-    if(razaoSocial.value != "" && email.value != "" && CEP.value != '' 
+    if(razaoSocial.value != "" && validarEmail(email.value) && CEP.value != '' 
     && logradouro.value != '' && CNPJ.value != '' 
-    && numTelefone.value != ''){
+    && numTelefone.value != '')
+    {
 
         var pessoa = {
             razaoSocial: razaoSocial.value,
